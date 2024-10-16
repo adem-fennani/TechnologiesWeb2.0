afbab3af5df2066234d97d1fa587a32262f99dea
adem-fennani <adem.fennani@esprit.tn>
2024-10-16 10:41:20 +0100

    v1.9 (feat: Added Search functionality)

--- src/Controller/CrudAuthorController.php
+++ src/Controller/CrudAuthorController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,5 +17,19 @@ class CrudAuthorController extends AbstractController
         $list = $repository->findAll();
         return $this->render("crud_author/list.html.twig",
         ["list"=>$list]);
+    }
+
+    // Method to search an author by name
+    #[Route('/search/{name}', name: 'app_crud_search')]
+    public function searchByName(AuthorRepository $repository, Request $request) : Response {
+        // Get the data name from the request
+        $name = $request->get("name");
+
+        // Use a magic method to make a search by name
+        $authors = $repository->findByName($name);
+        // var_dump($authors);
+        // die();
+        return $this->render("crud_author/list.html.twig",
+        ["list"=>$authors]);
     }
 }
