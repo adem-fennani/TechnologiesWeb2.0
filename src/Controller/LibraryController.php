ef5d12580fdc753ccd5c62b0bdbc0502df4cfcbf
adem-fennani <adem.fennani@esprit.tn>
2024-10-23 22:44:12 +0100

    v2.5 (feat: Added Library CRUD)

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -47,4 +47,30 @@ class LibraryController extends AbstractController
 
         return $this->redirectToRoute("app_library_list");
     }
+
+    // Method to delete a library
+    #[Route('/delete/{id}', name: 'app_library_delete')]
+    public function deleteAuthor(Library $library, ManagerRegistry $doctrine): Response {
+        $em = $doctrine->getManager();
+        $em->remove($library);
+        $em->flush();
+        
+        return $this->redirectToRoute("app_library_list");
+    }
+
+    // Method to update an author
+    #[Route('/update/{id}', name: 'app_library_update')]
+    public function updateAuthor(Library $library, ManagerRegistry $doctrine): Response {
+        $library->setName("Farid");
+        $library->setWebsite("farid.library.tn");
+        $creationDate = new \DateTime('2024-09-29');
+        $library->setCreationDate($creationDate);
+
+        // Persist the object in the doctrine
+        $em = $doctrine->getManager();
+        $em->persist($library);
+        $em->flush();
+
+        return $this->redirectToRoute("app_library_list");
+    }
 }
